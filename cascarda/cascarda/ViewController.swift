e6220dadb7ceb15620cbc509d024b0aa1e1f9a72
v.kitchenko <v.kitchenko@ltst.ru>
2021-10-18 13:03:13 +0600

    first commit

---
+++ cascarda/cascarda/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cascarda
+//
+//  Created by Вениамин Китченко on 18.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
